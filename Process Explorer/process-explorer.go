f4bee0d93d3a9bc8c6e7251811dbfc127129401c
Rohan Likhite <rlikhite@corp.entertechsystems.com>
2017-03-09 13:54:38 -0500

    added toggle to show/hide system processes

--- Process Explorer/process-explorer.go
+++ Process Explorer/process-explorer.go
@@ -6,11 +6,12 @@ import (
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func getProcesses() string {
+func getProcesses(defaultProcesses bool) string {
 	processes, err := ps.Processes()
 
 	if err != nil {
@@ -20,7 +21,12 @@ func getProcesses() string {
 	var output_string string
 
 	for _, process := range processes {
-		output_string += fmt.Sprintf("\n%d - %s: \n - Running for: %s \n - PPID:[%d]\n", process.Pid(), process.Executable(), processDuration(process), process.PPid())
+		duration := processDuration(process)
+		if defaultProcesses && duration != "2562047h47m16.854775807s" {
+			output_string += fmt.Sprintf("\n%d - %s: \n - Running for: %s \n - PPID:[%d]\n", process.Pid(), process.Executable(), duration, process.PPid())
+		} else if !defaultProcesses {
+			output_string += fmt.Sprintf("\n%d - %s: \n - Running for: %s \n - PPID:[%d]\n", process.Pid(), process.Executable(), duration, process.PPid())
+		}
 	}
 
 	return output_string
@@ -36,6 +42,8 @@ func processDuration(process ps.Process) string {
 func main() {
 
 	var applications *walk.TextEdit
+	var toggleDefaultsCheckBox *walk.CheckBox
+	showDefaultProcesses := false
 
 	MainWindow{
 		Title:   "Application Stats",
@@ -43,17 +51,32 @@ func main() {
 		Layout:  VBox{},
 		Children: []Widget{
 			HSplitter{
+				MinSize: Size{300, 570},
 				Children: []Widget{
 					TextEdit{AssignTo: &applications, ReadOnly: true},
 				},
 			},
-			PushButton{
-				Text: "Get Data",
-				OnClicked: func() {
-					applications.SetText("")
-					for _, applicationString := range strings.Split(getProcesses(), "\n") {
-						applications.AppendText(applicationString + "\r\n")
-					}
+			HSplitter{
+				Children: []Widget{
+					CheckBox{
+						AssignTo: &toggleDefaultsCheckBox,
+						Text:     "Hide Defaults",
+						Checked:  false,
+						OnCheckStateChanged: func() {
+							showDefaultProcesses = !showDefaultProcesses
+							checkboxOutput := fmt.Sprintf("Hide System Processes: %s", strconv.FormatBool(showDefaultProcesses))
+							applications.SetText(checkboxOutput)
+						},
+					},
+					PushButton{
+						Text: "Get Data",
+						OnClicked: func() {
+							applications.SetText("")
+							for _, applicationString := range strings.Split(getProcesses(showDefaultProcesses), "\n") {
+								applications.AppendText(applicationString + "\r\n")
+							}
+						},
+					},
 				},
 			},
 		},
